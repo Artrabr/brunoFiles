310e38789c1329ddfef2fe0bdda7fda5608d6270
Artarbr <arthurrabello2009@gmail.com>
2026-09-25 19:21:52 -0300

    player sprite animation + movimentation

--- scripts/script_player/Player.gd
+++ scripts/script_player/Player.gd
@@ -1,27 +1,71 @@
 class_name Player
 extends CharacterBody2D
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-#=============Modificadores==================
-var m_str:             int = 5 #forca       I
-var m_dex:             int = 5 #destreza    I
-var m_con:             int = 5 #constituiçãoI
-var m_int:             int = 5 #inteligenciaI
-var m_cha:             int = 5 #carisma     I
-#============================================
-
+# ============ DADOS ============
 var character_name: String = "Michael"
-var age: int = 16
-var spd: int = 5
+
+var m_str: int = 5
+var m_dex: int = 5
+var m_con: int = 5
+var m_int: int = 5
+var m_cha: int = 5
+
+var spd: float = m_dex*50
 
 var base_hp: int = 50
 var max_hp: int = get_max_hp()
 var hp: int = max_hp
 
+# ============ VIDA ============
 
 func get_max_hp() -> int:
-	var output: int = base_hp + (m_con * 10)
-	return output
+	return base_hp + (m_con * 10)
 
+# ============ MOVIMENTO ============
+
+func get_direction() -> Vector2:
+	var direction: Vector2 = Vector2.ZERO
+
+	if Input.is_key_pressed(KEY_W):
+		direction.y -= 1
+
+	if Input.is_key_pressed(KEY_S):
+		direction.y += 1
+
+	if Input.is_key_pressed(KEY_A):
+		direction.x -= 1
+
+	if Input.is_key_pressed(KEY_D):
+		direction.x += 1
+
+	return direction.normalized()
+
+func change_sprite_movement(direction: Vector2) -> void: #alterar o sprite caminhando
+	if direction.x < 0:
+		sprite.play("walk_left")
+
+	elif direction.x > 0:
+		sprite.play("walk_right")
+
+	elif direction.y < 0:
+		sprite.play("walk_up")
+
+	elif direction.y > 0:
+		sprite.play("walk_down")
+
+	elif direction.y == 0:
+		sprite.play("idle")
+
+func _physics_process(_delta: float) -> void:
+	var direction: Vector2 = get_direction()
+
+	velocity = direction * spd
+	change_sprite_movement(direction)
+
+	move_and_slide()
+
+# ============ INICIALIZAÇÃO ============
 
 func _ready() -> void:
 	print("HP atual: ", hp)
